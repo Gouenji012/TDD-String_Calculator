3305173313c19480b02b51ec5053b099853c3061
manishkumar <gautammanish012@gmail.com>
2025-05-14 16:01:42 +0530

    writing code for adding numbers for single, multiple and in line

--- src/main/java/org/incubyte/KataStringCalculatorTDD.java
+++ src/main/java/org/incubyte/KataStringCalculatorTDD.java
@@ -2,8 +2,46 @@ package org.incubyte;
 
 public class KataStringCalculatorTDD {
 
-    public int addString(String numbers)
-    {
+    public int addString(String numbers) throws IllegalAccessException {
+        if(numbers.length()<2)
+        {
+            if(numbers.isEmpty()) return 0;
+            else return Integer.parseInt(numbers);
+        }
+        else {
+            String del = ",";
+            if(numbers.matches("//(.*)\n(.*)")){
+                del = Character.toString(numbers.charAt(2));
+                numbers=numbers.substring(4);
+            }
 
+            String[] numberList = splitNumbers(numbers,del+"|\n");
+            return addition(numberList);
+        }
+
+    }
+
+    private String[] splitNumbers(String num, String div)
+    {
+        return num.split(div);
+    }
+
+    private int addition(String[] num) throws IllegalAccessException {
+        var addition = 0;
+        StringBuilder neg = new StringBuilder();
+
+        for (String str : num) {
+            if (Integer.parseInt(str) < 0) {
+                if (neg.toString().equals(""))
+                    neg = new StringBuilder(str);
+                else
+                    neg.append(",").append(str);
+            }
+            if (Integer.parseInt(str) < 1000) {
+                addition += Integer.parseInt(str);
+            }
+
+        }
+        return addition;
     }
 }
