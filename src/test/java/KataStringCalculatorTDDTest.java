bd480842d9608016c3a1f101d3755223a0c9fbb1
manishkumar <gautammanish012@gmail.com>
2025-05-14 15:55:48 +0530

    writing test cases for adding empty string, one number and multiple numbers

--- src/test/java/KataStringCalculatorTDDTest.java
+++ src/test/java/KataStringCalculatorTDDTest.java
@@ -1,6 +1,9 @@
 import org.incubyte.KataStringCalculatorTDD;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class KataStringCalculatorTDDTest {
 
@@ -15,6 +18,26 @@ public class KataStringCalculatorTDDTest {
     @AfterEach
     public void destroy() {
         kataStringCalculatorTDD=null;
+    }
+
+    @Test
+    public void testEmptyString() throws IllegalAccessException {
+        assertEquals(0,kataStringCalculatorTDD.addString(""));
+    }
+
+    @Test
+    public void testAddOneNumber() throws IllegalAccessException {
+        assertEquals(30,kataStringCalculatorTDD.addString("30"));
+    }
+
+    @Test
+    public void testTwoNumbers() throws IllegalAccessException {
+        assertEquals(50,kataStringCalculatorTDD.addString("30,20"));
+    }
+
+    @Test
+    public void testMultipleNumbers() throws IllegalAccessException {
+        assertEquals(100,kataStringCalculatorTDD.addString("20,55,25"));
     }
 
 
